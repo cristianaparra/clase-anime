51738b6712e129bb0a6aa747d96729e51ae78e50
José Ramón Vergara Ariztía <jrvergara21@gmail.com>
2020-08-21 21:40:00 -0400

    Agregar formulario y caja de Texto

--- src/vista/Home.js
+++ src/vista/Home.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 const Home = () => {
+    
+    const NuevaFuncion =(event) =>{
+        event.preventDefault();
+        console.log(event.target[0].value);
+        console.log(event.target.elements.filter.value);
+
+    } 
 
     return (
         <>
-        
+        <form onSubmit={NuevaFuncion}>
+            <input type="text" name="filter" className="form-control"/>
+            <button type="submit" className="btn btn-primary">Buscar</button>
+        </form>
             home
         </>
     )
+ 
 }
 
 
